33a4eaa2d6477d40bb25e2b03039410fef13c089
tsh <dr.tallin@gmail.com>
2017-03-05 18:13:06 +0200

    BinaryTree

---
+++ search/binary_tree.py
@@ -0,0 +1,38 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def add(self, value):
+        if value <= self.value:
+            if self.left:
+                self.left.add(value)
+            else:
+                self.left = Node(value)
+        else:
+            if self.right:
+                self.right.add(value)
+            else:
+                self.right = Node(value)
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, value):
+        if self.root:
+            self.root.add(value)
+        else:
+            self.root = Node(value)
+
+
+if __name__ == '__main__':
+    bt = BinaryTree()
+    bt.add(10)
+    assert bt.root.value == 10
+    bt.add(7)
+    assert bt.root.left.value == 7
+    bt.add(12)
+    assert bt.root.right.value == 12
