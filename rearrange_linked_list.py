150a11951cde0687304fdb3bf75f89b6fe5229d2
Anton Nikulin <dr.tallin@gmail.com>
2022-05-25 19:51:50 +0300

    initial

---
+++ rearrange_linked_list.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+  def print_list(self):
+    temp = self
+    while temp is not None:
+      print(str(temp.value) + " ", end='')
+      temp = temp.next
+    print()
+
+
+def reorder(head):
+  # TODO: Write your code here
+  return
+
+
+def main():
+  head = Node(2)
+  head.next = Node(4)
+  head.next.next = Node(6)
+  head.next.next.next = Node(8)
+  head.next.next.next.next = Node(10)
+  head.next.next.next.next.next = Node(12)
+  reorder(head)
+  head.print_list()
+
+
+main()
