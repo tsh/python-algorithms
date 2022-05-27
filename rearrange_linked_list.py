351e87d0ac77fba4c2b73afe0c350fdf4b1b1eef
Anton Nikulin <dr.tallin@gmail.com>
2022-05-27 17:07:18 +0300

    rearrange linked list

--- rearrange_linked_list.py
+++ rearrange_linked_list.py
@@ -13,9 +13,38 @@ class Node:
             temp = temp.next
         print()
 
+    def __repr__(self):
+        return f'N: {self.value} -> {self.next}'
+
 
 def reorder(head):
-    return
+    # find middle
+    slow, fast = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    # merge
+    first_half = head
+    second_half = reverse(slow)
+    while first_half and second_half:
+        temp = first_half.next
+        first_half.next = second_half
+        first_half = temp
+
+        temp = second_half.next
+        second_half.next = first_half
+        second_half = temp
+    if first_half is not None:
+        first_half.next = None
+
+def reverse(head):
+    prev = None
+    while head:
+        next_ = head.next
+        head.next = prev
+        prev = head
+        head = next_
+    return prev
 
 
 def main():
