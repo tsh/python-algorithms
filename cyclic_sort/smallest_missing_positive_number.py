2723603949ad9babc8dc0dc0c70137bd492e65ea
tsh <dr.tallin@gmail.com>
2022-06-13 21:50:03 +0300

    initial

---
+++ cyclic_sort/smallest_missing_positive_number.py
@@ -0,0 +1,11 @@
+"""
+Given an unsorted array containing numbers, find the smallest missing positive number in it.
+Note: Positive numbers start from ‘1’.
+"""
+
+
+def find_first_smallest_missing_positive(nums):
+    i, n = 0, len(nums)
+
+
+print('3: ', find_first_smallest_missing_positive([-3, 1, 5, 4, 2]))
