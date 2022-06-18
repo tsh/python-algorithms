8bb2726f2c5220b53a6028a48f686467f26ebbc3
tsh <dr.tallin@gmail.com>
2022-06-18 20:38:19 +0300

    initial

---
+++ linked_list/reverse.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+"""
+Given the head of a Singly LinkedList, reverse the LinkedList.
+Write a function to return the new head of the reversed LinkedList.
+"""
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse(head):
+    prev = None
+    cur = head
+    while cur:
+        pass
+    return head
+
+
+def main():
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()
